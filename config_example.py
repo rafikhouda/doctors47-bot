bdb2dd0722717ecaec0cf1ad356706a661c3c4f7
rafikhouda <78047673+rafikhouda@users.noreply.github.com>
2025-11-17 17:25:24 +0100

    Update config_example.py

--- config_example.py
+++ config_example.py
@@ -1,5 +1,5 @@
 # Example configuration file. Do NOT commit your real token to GitHub.
 
-BOT_TOKEN = "YOUR_TELEGRAM_BOT_TOKEN_HERE"
+BOT_TOKEN = "8532347892:AAFvS6oLnVfCZXwzMtmOIdecuRwNe_oibCQ"
 # ADMIN_IDS: list of numeric Telegram user IDs allowed to add/delete doctors
 ADMIN_IDS = [123456789]
